93b9a27c78e5d7da8a908fefd2ef23d6a8216492
Nicola Mustone <nicola@mustone.blog>
2026-06-28 18:49:18 +0300

    added experience section

---
+++ src/data/jobs.ts
@@ -0,0 +1,33 @@
+export interface Job {
+  years: string;
+  role: string;
+  company: string;
+  description?: string;
+}
+
+export const jobs = [
+  {
+    years: "2022 - Today",
+    role: "Team Lead",
+    company: "Automattic Inc.",
+    description: "",
+  },
+  {
+    years: "2014 - 2022",
+    role: "Sr Support Agent",
+    company: "Automattic Inc. & WooThemes",
+    description: "",
+  },
+  {
+    years: "2012 - 2014",
+    role: "Jr. Support Agent & WP Dev",
+    company: "YITH",
+    description: "",
+  },
+  {
+    years: "2010 - 2012",
+    role: "Web Dev",
+    company: "Freelance",
+    description: "",
+  },
+];
